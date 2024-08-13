7e40d3e3d13e89ba11830447a895812fdaad172b
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-13 14:38:44 +0300

    completed the type annotatioon

--- checkpoint-5.playground/Contents.swift
+++ checkpoint-5.playground/Contents.swift
@@ -3,8 +3,12 @@
 let LuckyNumbers : [Int] = [7,4,38,21,16,15,12,33,31,49]
 
 //filtering the even numbers :
-var result: [()] = LuckyNumbers.filter{
+var result : [String] = LuckyNumbers.filter{
     $0%2 != 0
 }.sorted(by: <).map{
-    print("\($0) is a lucky number")
+    "\($0) is a lucky number"
 }
+for i in result{
+    print(i)
+}
+print(result)
