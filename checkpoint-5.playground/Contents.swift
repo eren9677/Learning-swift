df1bf020a6ee730ce25006344368ae4b763c3a70
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-13 14:34:27 +0300

    more optimal code for the checkpoint 5

--- checkpoint-5.playground/Contents.swift
+++ checkpoint-5.playground/Contents.swift
@@ -3,15 +3,8 @@
 let LuckyNumbers : [Int] = [7,4,38,21,16,15,12,33,31,49]
 
 //filtering the even numbers :
-var result : [Int] = LuckyNumbers.filter{
+var result: [()] = LuckyNumbers.filter{
     $0%2 != 0
-}.sorted(by: <)
-//sprting the array in ascending order:
-
-var answer : [String] = result.map{
-    "\($0) is a lucky number"
-}
-
-for a in answer{
-    print("\(a)")
+}.sorted(by: <).map{
+    print("\($0) is a lucky number")
 }
