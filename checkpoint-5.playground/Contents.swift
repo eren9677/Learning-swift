aada180163a8471493313163e86951abd68631aa
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-13 14:32:01 +0300

    completed the checkpoint 5

--- checkpoint-5.playground/Contents.swift
+++ checkpoint-5.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+//this check point is to find the lucky numbers inside an array.
 
-var greeting = "Hello, playground"
+let LuckyNumbers : [Int] = [7,4,38,21,16,15,12,33,31,49]
+
+//filtering the even numbers :
+var result : [Int] = LuckyNumbers.filter{
+    $0%2 != 0
+}.sorted(by: <)
+//sprting the array in ascending order:
+
+var answer : [String] = result.map{
+    "\($0) is a lucky number"
+}
+
+for a in answer{
+    print("\(a)")
+}
