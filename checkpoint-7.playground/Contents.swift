70fe5c8b29f61b37428257b7643e8da539db47c2
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-20 14:03:30 +0300

    i've completed the 7th challenge

--- checkpoint-7.playground/Contents.swift
+++ checkpoint-7.playground/Contents.swift
@@ -1,3 +1,46 @@
-import UIKit
+class Animal {
+    var legs : Int = 4
+    
+    
+}
 
-var greeting = "Hello, playground"
+class Dog : Animal {
+    func speak(){
+        print("Bark! Imma dog ")
+    }
+}
+
+final class Golden : Dog {
+    override func speak(){
+        print("Bark imma golden")
+    }
+}
+
+class  Cat : Animal {
+    var isTame : Bool  = false
+    
+    init( isTame: Bool) {
+        self.isTame = isTame
+      }
+
+    func speak (){
+        print("Meoww! ik ben een katze ! an ik heb \(super.legs) leggen")
+    }
+}
+final class Persian : Cat {
+    override func speak(){
+        print("meow but actually we are not arabs i also have \(super.legs) legs")
+    }
+}
+
+
+
+let oreo = Golden()
+oreo.speak() // aaarf
+
+let mocha = Persian(isTame: true)
+mocha.speak() // meeoOow
+print(mocha.isTame) // true
+Dog().speak()
+Golden().speak()
+Cat(isTame: true).speak()
