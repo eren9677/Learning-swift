affc3d38f6212c6113c42524f527c480426a43ee
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-21 15:44:34 +0300

    a

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scale : Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                scale += 1
+                
+            } label: {
+                Label(
+                    title: { Text("Hello World!").font(.title3) },
+                    icon: { Image(systemName: "snowflake").imageScale(.large) }
+                )
+            }
+            .symbolEffect(.bounce, value: scale)
+            .scaledToFit()
+            
+            Text("Button is clicked for \(scale) times")
+            
+            
         }
-        .padding()
     }
 }
 
