24e67cf078ad6b149d8e33ca7184c84e1ec36a19
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-22 13:25:35 +0300

    picker stylşe chanegd

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,15 +10,33 @@ import SwiftUI
 struct ContentView: View {
     @State private var check: Double = 0.0
     @State private var people: Int = 2 // Minimum amount of a split made is two.
-    @State private var tipPercentage: Int = 15
+    @State private var tipPercentage: Int = 10
     
-    let tipPercentages: [Int] = [0, 15, 20, 25, 30]
+    let tipPercentages: [Int] = [0, 10, 15, 20, 25]
     
     var body: some View {
         NavigationView {
             Form {
-                TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "YEN" ))
-                    .keyboardType(.decimalPad)
+                Section{
+                    TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "YEN" ))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $people) {
+                        ForEach(2 ..< 11 ,id: \.self){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                }
+                
             }
             .navigationTitle("WeSplit")
         }
