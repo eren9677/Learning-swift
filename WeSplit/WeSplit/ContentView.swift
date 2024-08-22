098f135a0f960a4d26a21c275a6b8f83b9a8b920
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-22 13:58:35 +0300

    finished the wesplit app

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -35,7 +35,8 @@ struct ContentView: View {
                         }
                         .pickerStyle(.segmented)
                     }
-                }
+                    .pickerStyle(.segmented)
+                
                 
             }
             .navigationTitle("WeSplit")
