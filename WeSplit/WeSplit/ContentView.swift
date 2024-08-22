1a7915efcbd6b056e6fb054d3bd5ba3eb4b52638
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-22 13:59:01 +0300

    changed currency

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,14 +11,17 @@ struct ContentView: View {
     @State private var check: Double = 0.0
     @State private var people: Int = 2 // Minimum amount of a split made is two.
     @State private var tipPercentage: Int = 10
-    
     let tipPercentages: [Int] = [0, 10, 15, 20, 25]
+    
+    private var totalPerPerson : Double {
+        return (check + (check * Double(tipPercentage) / 100)) / Double(people)
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section{
-                    TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "YEN" ))
+                    TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
                         .keyboardType(.decimalPad)
                     
                     Picker("Number of people", selection: $people) {
@@ -28,15 +31,21 @@ struct ContentView: View {
                     }
                 }
                 
-                Section{
+                Section("How much do you want to tip?"){
                     Picker("Tip percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self){
                             Text($0, format: .percent)
                         }
-                        .pickerStyle(.segmented)
                     }
                     .pickerStyle(.segmented)
+                    
+                }
                 
+                Section("Payment per person is : "){
+                    
+                    Text(totalPerPerson ,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                }
                 
             }
             .navigationTitle("WeSplit")
