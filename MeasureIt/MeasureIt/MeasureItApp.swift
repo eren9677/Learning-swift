a8e10fb5bd53fd213d52fe0bfa7c890ad2bf5d37
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-23 11:12:16 +0300

    completed the first version of MeasureIt

---
+++ MeasureIt/MeasureIt/MeasureItApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureItApp.swift
+//  MeasureIt
+//
+//  Created by eren on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeasureItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
