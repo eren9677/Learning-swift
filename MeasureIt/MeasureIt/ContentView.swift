0897caf0ca4a3e6b0e871050cc1aa245b232d42b
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-23 15:49:46 +0300

    .

--- MeasureIt/MeasureIt/ContentView.swift
+++ MeasureIt/MeasureIt/ContentView.swift
@@ -13,28 +13,50 @@ struct ContentView: View {
     @State private var measureType : String = "km"
     @State private var measuredType : String = "m"
     
-    private let conversionTable : [String : [String : Double]] = [
-        
-        "m" : ["m" : 1, "km" : 0.001, "foot": 3.2808, "yard": 1.0936, "mil": 0.0006],
-        "km" : ["m" :1000, "km": 1, "foot": 3280.8398, "yard": 1093.6132,"mil":  0.6213 ],
-        "foot": ["m": 0.3048, "km": 0.0003, "foot": 1, "yard": 0.3333, "mil": 0.0001 ],
-        "yard": ["m": 0.9144, "km": 0.0009144, "foot": 3, "yard": 1, "mil": 0.00056],
-        "mil": ["m": 1609.344, "km": 1.609344, "foot": 5280, "yard": 1760, "mil": 1]
-    
-    ]
+//    private let conversionTable : [String : [String : Double]] = [
+//
+//        "m" : ["m" : 1, "km" : 0.001, "foot": 3.2808, "yard": 1.0936, "mil": 0.0006],
+//        "km" : ["m" :1000, "km": 1, "foot": 3280.8398, "yard": 1093.6132,"mil":  0.6213 ],
+//        "foot": ["m": 0.3048, "km": 0.0003, "foot": 1, "yard": 0.3333, "mil": 0.0001 ],
+//        "yard": ["m": 0.9144, "km": 0.0009144, "foot": 3, "yard": 1, "mil": 0.00056],
+//        "mil": ["m": 1609.344, "km": 1.609344, "foot": 5280, "yard": 1760, "mil": 1]
+//
+//    ]
     private var measuredVal : Double {
-        return CalculateMeasure(measure: measureVal, mt1: measureType, mt2: measuredType)
+        return CalculateFaster(measure: measureVal, mt1: measureType, mt2: measuredType)
+    }
+    
+    private let reducedConversionTable : [String: Double] = [
+        "m": 1,
+        "km": 1000,
+        "foot": 0.3048,
+        "yard": 0.9144,
+        "mil": 1609.344
+        
+    ]
+    
+    private func CalculateFaster(measure: Double, mt1: String, mt2: String) -> Double{
+        // this funcion uses linar dimensional reduction.
+        //  in order to convert C[i][j] while converting to i to j ;  =  we first convert i to m then converting back to j linear cebir babbyyyy
+        
+        
+        if let conversionRates = reducedConversionTable[mt1], let conversionRate = reducedConversionTable[mt2] {
+            return (measure * conversionRates)/conversionRate
+        }
+        else {
+            return 0.0
+        }
     }
 
     
     
-    private func CalculateMeasure(measure: Double, mt1: String, mt2: String) -> Double {
-        if let conversionRates = conversionTable[mt1], let conversionRate = conversionRates[mt2] {
-            return measure * conversionRate
-        } else {
-            return 0.0
-        }
-    }
+//    private func CalculateMeasure(measure: Double, mt1: String, mt2: String) -> Double {
+//        if let conversionRates = conversionTable[mt1], let conversionRate = conversionRates[mt2] {
+//            return measure * conversionRate
+//        } else {
+//            return 0.0
+//        }
+//    }
 
     private let measureTypes : [String] = ["m","km","foot","yard","mil"]
     
@@ -77,7 +99,7 @@ struct ContentView: View {
                     
                 }
                 
-                Text("\(measureVal.formatted()) \(measureType) is \(measuredVal.formatted()) \(measuredType)")
+                Text("\(measureVal.formatted(.number.precision(.fractionLength(2)))) \(measureType) is \(measuredVal.formatted(.number.precision(.fractionLength(2)))) \(measuredType)")
                 
                 
             }
