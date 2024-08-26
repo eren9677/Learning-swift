8f532124514e433110d3a969d76b1be980d5fe61
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-26 11:48:28 +0300

    changed the view and syles

--- GuessFlags/GuessFlags/ContentView.swift
+++ GuessFlags/GuessFlags/ContentView.swift
@@ -7,39 +7,35 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        
-        ZStack{
-            Color(red:1, green:0.8, blue:0)
-            VStack{
-                ForEach(1..<4){_ in
-                    HStack{
-                        ForEach(1..<4) {_ in
-                            
-                            Image(systemName: "globe")
-                                .imageScale(.large)
-                                .foregroundStyle(.tint)
-                        }
-                    }
-                    
-                }
-            }.background(.ultraThinMaterial)
-            
-            VStack{
-                Spacer()
-                Spacer()
-                Button("Click to me"){
-                    print("button was clicked")
-                }
-                    .buttonStyle(.bordered)
-                Spacer()
-            }
-            
 
+struct ContentView: View {
+    
+    private var countries : [String] = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "Ukraine", "US"]
+    private var corectCountry : Int = Int.random(in: 0...2)
+    
+    
+    
+    var body: some View {
+        ZStack{
+            Color(red :1, green: 0.5, blue: 0 ).ignoresSafeArea()
+            VStack(spacing:30){
+                VStack(){
+                    Text("Select the flag of").foregroundStyle(.white)
+                    Text(countries[corectCountry]).foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3){ number in
+                    Button(){
+                        
+                    }label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-        .ignoresSafeArea()
     }
+        
+        
 }
 
 #Preview {
