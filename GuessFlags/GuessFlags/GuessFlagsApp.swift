2cccb2f0aebf0f7583fa7c1421c1b4b82b87e4d9
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-26 11:11:26 +0300

    created the project

---
+++ GuessFlags/GuessFlags/GuessFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagsApp.swift
+//  GuessFlags
+//
+//  Created by eren on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
