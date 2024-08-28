aedf123840c76a6c511c47495b4dd032ecef5b2c
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-08-28 11:01:54 +0300

    brain game for rock papers and scissors

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by eren on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
