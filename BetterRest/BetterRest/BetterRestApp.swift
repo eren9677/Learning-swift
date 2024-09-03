d16f211db51e87da9579dccd7adecfd7b814aa08
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-03 09:02:27 +0300

    betterRest app initializaation

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by eren on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
