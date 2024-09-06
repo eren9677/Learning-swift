9119eb5c3b5e694a50cd6d1cab8a85195b6b715b
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-06 10:59:42 +0300

    WordScramble app and it's bundles

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by eren on 6.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var usedWords : [String] = [String]()
+    
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                Section{
+                    TextField("Please Enter your word", text: $newWord)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self){word in
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+        }
+    }
+    
+    func testUrls() {
+        if let bundleUrl = Bundle.main.url(forResource: "deneme", withExtension: "txt"){
+            
+            if let bundleContent = try? String(contentsOf: bundleUrl){
+                print("content is found : \(bundleContent)")
+            }
+            else{
+                print("there is no content in bundle!")
+            }
+            
+        }else{
+            print("Bundle url can not be found !")
+        }
+    }
+    
+    
+    func testStrings(){
+        var word = "swift"
+        var checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelled = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "EN")
+        
+        let allGood = misspelled.location == NSNotFound
+        
+        //this code checks if a misspell is found or not in a range
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by eren on 6.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
