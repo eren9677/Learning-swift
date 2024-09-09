36334fc9eeff96ef1580d4f13e27a6b5e85065cc
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-09 13:44:46 +0300

    app initialization

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by eren on 9.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by eren on 9.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
