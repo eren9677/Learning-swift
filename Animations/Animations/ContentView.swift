0e82f1f1701328d58f9cb0eb14f0f701ed89c796
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-10 10:17:06 +0300

    dragging gesture with animation is added .

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount : Double = 1.0
+    
+    @State private var dragAmount : CGSize = CGSize.zero
+    
     var body: some View {
         
         Button("Tap me"){
@@ -20,19 +23,39 @@ struct ContentView: View {
         .overlay(
             Circle()
                 .stroke(.red)
-                       .scaleEffect(animationAmount)
-                       .opacity(2 - animationAmount)
-                       .animation(
-                           .easeInOut(duration: 1)
-                               .repeatForever(autoreverses: false),
-                           value: animationAmount
-                       )
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
         )
         .onAppear{
             animationAmount = 2
         }
+        .padding()
+        
+        
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 20))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation}
+                    .onEnded{ _ in dragAmount = .zero}
+            )
+            .animation(
+                .spring(duration: 0.6, bounce: 0.6)
+                .repeatForever(autoreverses: false),
+                value: dragAmount
+            )
     }
-}
+        
+        
+        
+    }
 
 #Preview {
     ContentView()
