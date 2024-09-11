0cf645aa29401df7ee3c4cee4c480407b3f3a6bd
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-11 15:02:33 +0300

    i've experienxed sheets and how to pass data between two views

---
+++ IExpense/IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by eren on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
