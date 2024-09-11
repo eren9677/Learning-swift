41a9e0f8f1698c3330f265de559d60f229f6c1ba
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-11 15:03:08 +0300

    completed the challenge

--- IExpense/IExpense/ContentView.swift
+++ IExpense/IExpense/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
+ // this example wil show passing data between views using classes and an observable keyword.
+struct SecondView : View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("hello from second sheet!")
+        Button("dismiss"){
+            dismiss()
+        }
+    }
+}
+struct ContentView: View {
+    @State private var showingSheet : Bool = false
+    var body: some View {
+        
+        VStack{
+            Button("Show sheet"){
+                showingSheet.toggle()
+            }
         }
         .padding()
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView()
+        })
+            
     }
 }
 
