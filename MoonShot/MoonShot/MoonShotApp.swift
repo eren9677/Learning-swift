deb00f3c24b84940e6fe1ff6179dd09cf482f16d
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-09-17 14:01:29 +0300

    initial files of the moonshot app

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by eren on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
