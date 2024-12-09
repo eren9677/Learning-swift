1bd83722bd77b303ae8cce42ff8a346372868000
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-12-09 17:11:28 +0300

    Removed the extra view and re-aligned the toobar item with a comment.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -7,100 +7,6 @@
 
 import SwiftUI
 
-
-struct MainView : View {
-    
-    @State private var people: Int = 2
-    @State private var tipPercentage: Int = 10
-    @State private var tipAmount: Double = 0.0
-    @State private var tipPercentages: [Int] = [5, 10, 15, 20]
-    @State private var checkAmount: Double = 0.0
-    
-    private var checkPerPerson: Double {
-        return (checkAmount + (checkAmount * Double(tipPercentage) / 100)) / Double(people)
-    } // calculated property
-    
-    func calculateFinalAmount()-> Double{
-        return (checkAmount + (checkAmount * Double(tipPercentage) / 100)) / Double(people)
-    } //calculated function.
-    
-    @FocusState private var focused: Bool
-    
-    var body: some View {
-        
-        NavigationView{
-            Form {
-                
-                Section {
-                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($focused)
-                    
-                    Picker("Number of People", selection: $people){
-                        ForEach(2..<11, id:\.self){
-                            Text("\($0) people")
-                        }
-                    }
-                }
-                
-                Section ("Select the tip Percentage: ") {
-                    Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) { tipPercentage in
-                            Text("\(tipPercentage)%")
-                        }
-                    }.pickerStyle(.segmented)
-                }
-                
-                Section("Payment per person is : ") {
-                    Text(checkPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    
-                    Text(calculateFinalAmount(), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    
-                }
-            }
-            .navigationTitle("WeSplit")
-            .toolbar{
-                if focused {
-                    ToolbarItem(placement: .automatic) {
-                        Button("Done") {
-                            self.focused = false
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct ContentView: View {
     @State private var check: Double = 0.0
     @State private var people: Int = 2 // Minimum amount of a split made is two.
@@ -147,8 +53,14 @@ struct ContentView: View {
             .navigationTitle("WeSplit")
             .toolbar {
                 if amountIsFocused {
-                    Button("Done") {
-                        amountIsFocused = false
+                    ToolbarItem(placement: .keyboard){
+                        HStack {
+                            Text("Hope you enjoyed your meal 🍖")
+                            Spacer()
+                            Button("Done") {
+                                self.amountIsFocused = false
+                            }
+                        }
                     }
                 }
             }
@@ -157,5 +69,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    MainView()
+    ContentView()
 }
