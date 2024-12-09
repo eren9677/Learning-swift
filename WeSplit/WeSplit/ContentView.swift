be889537e203aa01ef0c201415463112ec5c6061
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2024-12-09 15:31:49 +0300

    Checked the differences between computed properties and functions with generics types.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -7,6 +7,100 @@
 
 import SwiftUI
 
+
+struct MainView : View {
+    
+    @State private var people: Int = 2
+    @State private var tipPercentage: Int = 10
+    @State private var tipAmount: Double = 0.0
+    @State private var tipPercentages: [Int] = [5, 10, 15, 20]
+    @State private var checkAmount: Double = 0.0
+    
+    private var checkPerPerson: Double {
+        return (checkAmount + (checkAmount * Double(tipPercentage) / 100)) / Double(people)
+    } // calculated property
+    
+    func calculateFinalAmount()-> Double{
+        return (checkAmount + (checkAmount * Double(tipPercentage) / 100)) / Double(people)
+    } //calculated function.
+    
+    @FocusState private var focused: Bool
+    
+    var body: some View {
+        
+        NavigationView{
+            Form {
+                
+                Section {
+                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($focused)
+                    
+                    Picker("Number of People", selection: $people){
+                        ForEach(2..<11, id:\.self){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section ("Select the tip Percentage: ") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { tipPercentage in
+                            Text("\(tipPercentage)%")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("Payment per person is : ") {
+                    Text(checkPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                    Text(calculateFinalAmount(), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                if focused {
+                    ToolbarItem(placement: .automatic) {
+                        Button("Done") {
+                            self.focused = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView: View {
     @State private var check: Double = 0.0
     @State private var people: Int = 2 // Minimum amount of a split made is two.
@@ -63,5 +157,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
